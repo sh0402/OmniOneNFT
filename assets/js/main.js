6f0f03115982adf17f27280d28dfadeb515cc6a3
shkim0060 <shkim0060@raoncorp.com>
2022-09-05 18:41:15 +0900

    220905_common_selectBox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+window.addEventListener('load', function() {
+  const allElements = document.getElementsByTagName('*');
+  Array.prototype.forEach.call(allElements, function(el) {
+      const includePath = el.dataset.includePath;
+      if (includePath) {
+          let xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function () {
+              if (this.readyState == 4 && this.status == 200) {
+                  el.outerHTML = this.responseText;
+              }
+          };
+          xhttp.open('GET', includePath, true);
+          xhttp.send();
+          return;
+      }
+  });
+});
+
 document.onscroll = headerFixed;
 
 function headerFixed() {
@@ -12,10 +30,57 @@ function headerFixed() {
 }
 
 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
+new Swiper(".heroSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
+  loopFillGroupWithBlank: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: "#hero-swiper .swiper-pagination",
     clickable: true,
   },
+  navigation: {
+    nextEl: "#hero-swiper .swiper-button-next",
+    prevEl: "#hero-swiper .swiper-button-prev",
+  },
 });
+
+new Swiper(".cardSwiper", {
+  slidesPerView: 4,
+  spaceBetween: 24,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: "#card-swiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: "#card-swiper .swiper-button-next",
+    prevEl: "#card-swiper .swiper-button-prev",
+  },
+});
+
+new Swiper(".prodSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: "#prod-swiper .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: "#prod-swiper .swiper-button-next",
+    prevEl: "#prod-swiper .swiper-button-prev",
+  },
+});
+
+document.addEventListener('click', function(){
+  const select = document.getElementById('select');
+
+  
+});
+
+
