bb02bed00110408746ccaf8c0478b2fdfdec271c
shkim0060 <shkim0060@raoncorp.com>
2022-09-14 18:30:30 +0900

    220914_table

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,8 +19,8 @@ window.addEventListener('load', function() {
 document.onscroll = headerFixed;
 
 function headerFixed() {
-  let header = document.querySelector('header');
-  let headerHeight = header.offsetHeight;
+  const header = document.querySelector('header');
+  const headerHeight = header.offsetHeight;
 
   if (window.scrollY > headerHeight){
     document.querySelector('header').classList.add('fixed');
@@ -45,19 +45,19 @@ new Swiper('.heroSwiper', {
   },
 });
 
-new Swiper('.cardSwiper', {
+new Swiper('.issuSwiper', {
   slidesPerView: 4,
   spaceBetween: 24,
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
-    el: '#card-swiper .swiper-pagination',
+    el: '#issu-swiper .swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: '#card-swiper .swiper-button-next',
-    prevEl: '#card-swiper .swiper-button-prev',
+    nextEl: '#issu-swiper .swiper-button-next',
+    prevEl: '#issu-swiper .swiper-button-prev',
   },
 });
 
